61de1047ac993a666e4ba4ad99b8130ff67c347c
Young Jin Yoon <yoon333@gmail.com>
2021-05-24 11:51:10 -0700

    Initial commit for own bdn9 commit
    Signed-off-by: Young Jin Yoon <yoon333@gmail.com>

---
+++ keyboards/keebio/bdn9/keymaps/yoon333/keymap.c
@@ -0,0 +1,146 @@
+/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+//TODO: get RGB per layer working with customized key code RGB_LYR
+enum encoder_names {
+  _LEFT,
+  _RIGHT,
+  _MIDDLE,
+};
+enum layers {
+  _ZERO,
+  _ONE,
+  SL_RGB, //special layer triggered by tap dance
+};
+
+enum {
+  TD_NO_MO0,
+  TD_MO1_RGB,
+};
+
+void td_no_mo0_reset(qk_tap_dance_state_t *state, void *user_data);
+void td_mo1_rgb_finished(qk_tap_dance_state_t *state, void *user_data);
+void td_mo1_rgb_reset(qk_tap_dance_state_t *state, void *user_data);
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_NO_MO0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, NULL, td_no_mo0_reset),
+  [TD_MO1_RGB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_mo1_rgb_finished, td_mo1_rgb_reset),
+};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+        | Knob 1: Media Next/Prev |          | Knob 2:  Vol Up/Dn |
+        | Press: Media Play/Pause | Home     | Press:        Mute |
+        |                Numpad 1 | Numpad 2 |           Numpad 3 |
+        |                Numpad 4 | Numpad 5 |           Numpad 6 |
+     */
+    [_ZERO] = LAYOUT(
+        KC_MPLY, TD(TD_MO1_RGB), KC_MUTE,
+        KC_KP_1,        KC_KP_2, KC_KP_3,
+        KC_KP_4,        KC_KP_5, KC_KP_6
+    ),
+    /*
+        | Knob 1: Media FF/RW |          | Knob 2: Media Vol Up/Dn |
+        | Press:   Media Stop |      N/A | Press:       Media Mute |
+        |            Numpad 7 | Numpad 8 |                Numpad 9 |
+        |            Numpad 0 | Numpad + |                Numpad - |
+     */
+    [_ONE] = LAYOUT(
+        KC_MSTP, TD(TD_MO1_RGB),     KC_STOP,
+        KC_KP_7,        KC_KP_8,     KC_KP_9,
+        KC_KP_0,     KC_KP_PLUS, KC_KP_MINUS
+    ),
+    /*
+        | Knob 1: RGB Mode +/- |           | Knob 2: RGB Sat Inc/Dec |
+        | Press:    RGB Toggle |       N/A | Press:        RGB Layer |
+        |             RGB Hue+ |  RGB Sat+ |		    RGB Val+ |
+        |             RGB Hue- |  RGB Sat- |                RGB Val- | 
+     */
+    [SL_RGB] = LAYOUT(
+         RGB_TOG, TD(TD_NO_MO0),   KC_NO,
+         RGB_HUI,       RGB_SAI, RGB_VAI, 
+         RGB_HUD,       RGB_SAD, RGB_VAD 
+    ),
+
+};
+
+const uint16_t PROGMEM encoders[][2][2] = {
+    [_ZERO] =   {{KC_MNXT, KC_MPRV}, {KC_VOLU, KC_VOLD}},
+    [_ONE]  =   {{KC_MFFD, KC_MRWD}, {KC_VOLU, KC_VOLD}},
+    //[SL_RGB]  = {{RGB_HUI, RGB_HUD}, {RGB_SAI, RGB_SAD}},
+    [SL_RGB]  = {{RGB_MOD, RGB_RMOD}, {KC_VOLU, KC_VOLD}},
+    // RGB defined in encoder_update_user()
+};
+
+const uint8_t PROGMEM layer_rgbs[][3] = {
+    [_ZERO] = {0x00, 0x00, 0xFF},
+    [_ONE] = {0x00, 0xFF, 0x00},
+    [SL_RGB] = {0x00, 0x00, 0xFF},
+};
+
+void td_no_mo0_reset(qk_tap_dance_state_t *state, void *user_data)
+{
+    if (state->count != 1) {
+        layer_off(SL_RGB);
+    }
+}
+
+
+void td_mo1_rgb_finished(qk_tap_dance_state_t *state, void *user_data)
+{
+    if (state->count == 1) {
+        layer_on(_ONE);
+    } 
+}
+void td_mo1_rgb_reset(qk_tap_dance_state_t *state, void *user_data)
+{
+    if (state->count == 1) {
+        layer_off(_ONE);
+    } else {
+	layer_on(SL_RGB);
+    }
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	uint8_t layer = get_highest_layer(layer_state);
+
+	uint8_t r = pgm_read_word(&layer_rgbs[layer][0]);
+	uint8_t g = pgm_read_word(&layer_rgbs[layer][0]);
+	uint8_t b = pgm_read_word(&layer_rgbs[layer][0]);
+
+	rgblight_setrgb(r, g, b);
+	return state;
+}
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    uint8_t layer = get_highest_layer(layer_state);
+    switch(layer) {
+    case SL_RGB:
+	if(index == _LEFT) {
+	    if(clockwise) {
+	        rgblight_increase();
+	    } else {
+	        rgblight_decrease();
+	    }
+	} else if (index == _RIGHT) {
+           //TODO: handle RGB layer knobs
+            tap_code16(pgm_read_word(&encoders[layer][index][clockwise]));
+	}
+	break;
+    default:
+        tap_code16(pgm_read_word(&encoders[layer][index][clockwise]));
+    }
+}
---
+++ keyboards/keebio/bdn9/keymaps/yoon333/rules.mk
@@ -0,0 +1,11 @@
+# Use macropad-specific defines, layers, functions. Flash bootloader with QM_MAKE
+IS_MACROPAD = yes
+
+# Use dfu bootloader for Elite-C
+BOOTLOADER = atmel-dfu
+
+# Build Options
+BACKLIGHT_ENABLE = no       # No backlights installed
+RGBLIGHT_ENABLE = yes
+RGBMATRIX_ENABLE = no
+TAP_DANCE_ENABLE = yes      # Use tap dance
