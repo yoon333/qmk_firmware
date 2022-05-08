6d78cf26a562705085989bd9ba1942c67ffada94
Young Jin Yoon <yoon333@gmail.com>
2022-05-08 12:01:31 -0700

    Added technik keymaps
    Signed-off-by: Young Jin Yoon <yoon333@gmail.com>

---
+++ keyboards/boardsource/technik_o/keymaps/yoon333/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#define PERMISSIVE_HOLD
+
+#ifdef MOUSEKEY_ENABLE
+    #define MOUSEKEY_INTERVAL 16
+    #define MOUSEKEY_DELAY 0
+    #define MOUSEKEY_TIME_TO_MAX 60
+    #define MOUSEKEY_MAX_SPEED 7
+    #define MOUSEKEY_WHEEL_DELAY 0
+#endif
+
+
